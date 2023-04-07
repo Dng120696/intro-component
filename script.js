a3e5debd221ba7b4e49883d40cda28b7ec73c7da
Dng16 <dongyang0016@gmail.com>
2023-04-08 07:45:40 +0800

    initial commit

--- script.js
+++ script.js
@@ -19,13 +19,19 @@ form.addEventListener('submit', (e) => {
     } 
     else{
       input[i].parentElement.classList.remove('error');
+      input[i].parentElement.classList.remove('valid-error');
       if(input[i].type === 'email'){
-        if(!isEmail(input[i].value)){
+        if(!input[i].value){
+          input[i].parentElement.classList.add('error');
+       
+        }else if(!isEmail(input[i].value)){
           input[i].parentElement.classList.add('valid-error');
-        }else{
-            input[i].parentElement.classList.remove('valid-error');
-
-         }      
+          
+         }else{
+      
+          input[i].parentElement.classList.remove('valid-error');
+         
+         }  
         }
 
       if(input[i].type === 'password'){
@@ -38,15 +44,12 @@ form.addEventListener('submit', (e) => {
         }
         
       }
+ 
     }
   } 
 });
 
-  function checkPassword(str)
-  {
-      var re = /^(?=.*\d)(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z]).{8,}$/;
-      return re.test(str);
-  }
+
 function isEmail(email) { 
   return /^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))$/i.test(email);
 } 
